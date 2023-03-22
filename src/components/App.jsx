e195b1f159e3da25a6bdf65a64a1c0ff221a2a3a
PaulinaStachPluskowska <109916798+PaulinaStachPluskowska@users.noreply.github.com>
2023-03-22 21:58:37 +0100

    update
    -test state commented
    -css changed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,18 +7,23 @@ import { ContactList } from "./Contacts/ContactList";
 import { Filter } from "./Filter/Filter";
 
 
-const INITIAL_STATE = {
-  contacts: [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ],
-  filter: '',
-};
+// const INITIAL_STATE = {
+//   contacts: [
+//     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+//     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+//     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+//     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+//   ],
+//   filter: '',
+// };
 
 export class App extends Component {
-  state = {...INITIAL_STATE};
+  // state = {...INITIAL_STATE};
+
+  state = {
+    contacts: [],
+    filter: '',
+  }
 
   handleSubmit = event => {
     event.preventDefault();
